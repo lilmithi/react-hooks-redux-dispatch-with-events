b8e17bbf004797cd1310d252fe0e66ad03cb9375
martin mithi <gathitumithi9000@gmail.com>
2022-12-21 13:00:01 +0300

    Completed assignment

--- src/reducer.js
+++ src/reducer.js
@@ -1,1 +1,25 @@
-// add code snippets from README
+let state;
+
+function reducer(state = { count: 0 }, action) {
+  switch (action.type) {
+    case "counter/increment":
+      return { count: state.count + 1 };
+    default:
+      return state;
+  }
+}
+
+function dispatch(action) {
+  state = reducer(state, action);
+  render();
+}
+
+function render() {
+  document.getElementById("container").textContent = state.count;
+}
+
+document
+  .getElementById("button")
+  .addEventListener("click", () => dispatch({ type: "counter/increment" }));
+
+dispatch({ type: "@@init" });
